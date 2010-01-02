38dec64c995cd4f1ffa199e0e6e6ff634b854117
unknown <Luc@.(none)>
2010-01-02 22:23:08 +0100

    debut modifs pour implantation de l'approche par minimisation d'une fonction quadratique

--- main.m
+++ main.m
@@ -53,8 +53,7 @@ clear all;close all;clc;
     matriceR.Rw=matriceR1.Rw;
     clear matriceR1;
     
-    % Calcul des valeurs observÃ©es
-    
+    % Calcul des valeurs observées    
     disp('IV  Calcul des valeurs observées')
     X=rand(size(ModePropreNorme.Vecteur,2),1);
     Vecteurp.orig=roundn(X,-3);
@@ -62,78 +61,89 @@ clear all;close all;clc;
     ModePropreObs=Calculobs(Vecteurp.orig,ModePropreNorme,matriceR);
     
     % Probleme inverse pour retrouver deltap à partir des valeurs propres
-    tol=10^-9; %tolérance (critère d'arrêt du gradient conjugué)
+    demarche='GCJ';     %%choix de l'approche: 'GCJ' gradient conjugué, 'QUAD' minimisation d'une fonction quadratique
+     
     
-    delta.worig=ModePropreObs.Valeur-ModePropreNorme.Valeur';
-    Vecteurp.recons=cgs(matriceR.Rw,delta.worig,tol);
-    ModePropreComp.Valeur= ModePropreObs.Valeur - matriceR.Rw*Vecteurp.recons;
-    Vecteurp_diff.orig=Vecteurp.recons-Vecteurp.orig;
-    Vecteurp_relat.orig=zeros(donnee.nelem,1);
-    for i=1:donnee.nelem
-        Vecteurp_relat.orig(i)=Vecteurp_diff.orig(i)/Vecteurp.orig(i);
+    disp('V  Résolution du pb inverse');
+    switch demarche;
+        case 'GCJ';
+            disp('>>>>> Méthode basée sur un gradient conjugué');
+            tol=10^-9; %tolérance (critère d'arrêt du gradient conjugué)
+
+            %%Données non bruitées
+            disp('>>>>> Données non bruitées');
+            delta.worig=ModePropreObs.Valeur-ModePropreNorme.Valeur';
+            Vecteurp.recons=cgs(matriceR.Rw,delta.worig,tol);
+            ModePropreComp.Valeur= ModePropreObs.Valeur - matriceR.Rw*Vecteurp.recons;
+            Vecteurp_diff.orig=Vecteurp.recons-Vecteurp.orig;
+            Vecteurp_relat.orig=zeros(donnee.nelem,1);
+            for i=1:donnee.nelem
+                Vecteurp_relat.orig(i)=Vecteurp_diff.orig(i)/Vecteurp.orig(i);
+            end
+            x=1:1:donnee.nelem;
+            figure;
+            bar(x,Vecteurp_diff.orig)
+            bar(x,Vecteurp_relat.orig)
+
+            % On bruite les données
+
+            % Entre 0 et 1%
+            disp('>>>>> Données bruitées (0% > 1%)');
+            ModePropreObs.Bruit1.Valeur = ModePropreObs.Valeur*(1+10^-2*rand(1,1));
+            delta.w1=ModePropreObs.Bruit1.Valeur-ModePropreNorme.Valeur';
+            Vecteurp.recons_bruit1=cgs(matriceR.Rw,delta.w1,tol);
+            Vecteurp_diff.bruit1=Vecteurp.recons_bruit1-Vecteurp.orig;
+            Vecteurp_relat.bruit1=zeros(donnee.nelem,1);
+            for i=1:donnee.nelem
+                Vecteurp_relat.bruit1(i)=Vecteurp_diff.bruit1(i)/Vecteurp.orig(i);
+            end
+
+            % Entre 0 et 5%
+            disp('>>>>> Données bruitées (0% > 5%)');
+            ModePropreObs.Bruit2.Valeur = ModePropreObs.Valeur*(1+5*10-2*rand(1,1));
+            delta.w2=ModePropreObs.Bruit2.Valeur-ModePropreNorme.Valeur';
+            Vecteurp.recons_bruit2=cgs(matriceR.Rw,delta.w2,tol);
+            Vecteurp_diff.bruit2=Vecteurp.recons_bruit2-Vecteurp.orig;
+            Vecteurp_relat.bruit2=zeros(donnee.nelem,1);
+            for i=1:donnee.nelem
+                Vecteurp_relat.bruit2(i)=Vecteurp_diff.bruit2(i)/Vecteurp.orig(i);
+            end
+
+            % Entre 0 et 10%
+            disp('>>>>> Données bruitées (0% > 10%)');
+            ModePropreObs.Bruit3.Valeur = ModePropreObs.Valeur*(1+10^-1*rand(1,1));
+            delta.w3=ModePropreObs.Bruit3.Valeur-ModePropreNorme.Valeur';
+            Vecteurp.recons_bruit3=cgs(matriceR.Rw,delta.w3,tol);
+            Vecteurp_diff.bruit3=Vecteurp.recons_bruit3-Vecteurp.orig;
+            Vecteurp_relat.bruit3=zeros(donnee.nelem,1);
+            for i=1:donnee.nelem
+                Vecteurp_relat.bruit3(i)=Vecteurp_diff.bruit3(i)/Vecteurp.orig(i);
+            end
+
+            % Entre 0 et 20%
+            disp('>>>>> Données bruitées (0% > 20%)');
+            ModePropreObs.Bruit4.Valeur = ModePropreObs.Valeur*(1+0.2*rand(1,1));
+            delta.w4=ModePropreObs.Bruit4.Valeur-ModePropreNorme.Valeur';
+            Vecteurp.recons_bruit4=cgs(matriceR.Rw,delta.w4,tol);
+            Vecteurp_diff.bruit4=Vecteurp.recons_bruit4-Vecteurp.orig;
+            Vecteurp_relat.bruit4=zeros(donnee.nelem,1);
+            for i=1:donnee.nelem
+                Vecteurp_relat.bruit4(i)=Vecteurp_diff.bruit4(i)/Vecteurp.orig(i);
+            end
+
+            % Entre 0 et 50%
+            disp('>>>>> Données bruitées (0% > 50%)');
+            ModePropreObs.Bruit5.Valeur = ModePropreObs.Valeur*(1+0.5*rand(1,1));
+            delta.w5=ModePropreObs.Bruit5.Valeur-ModePropreNorme.Valeur';
+            Vecteurp.recons_bruit5=cgs(matriceR.Rw,delta.w5,tol);
+            Vecteurp_diff.bruit5=Vecteurp.recons_bruit5-Vecteurp.orig;
+            Vecteurp_relat.bruit5=zeros(donnee.nelem,1);
+            for i=1:donnee.nelem
+                Vecteurp_relat.bruit5(i)=Vecteurp_diff.bruit5(i)/Vecteurp.orig(i);
+            end
+        case 'QUAD';
+            
     end
-    x=1:1:donnee.nelem;
-    figure;
-    bar(x,Vecteurp_diff.orig)
-    bar(x,Vecteurp_relat.orig)
-    
-    % On bruite les données
-    
-    % Entre 0 et 1%
-   
-    ModePropreObs.Bruit1.Valeur = ModePropreObs.Valeur*(1+10^-2*rand(1,1));
-    delta.w1=ModePropreObs.Bruit1.Valeur-ModePropreNorme.Valeur';
-    Vecteurp.recons_bruit1=cgs(matriceR.Rw,delta.w1,tol);
-    Vecteurp_diff.bruit1=Vecteurp.recons_bruit1-Vecteurp.orig;
-    Vecteurp_relat.bruit1=zeros(donnee.nelem,1);
-    for i=1:donnee.nelem
-        Vecteurp_relat.bruit1(i)=Vecteurp_diff.bruit1(i)/Vecteurp.orig(i);
-    end
-    
-    % Entre 0 et 5%
-    
-    ModePropreObs.Bruit2.Valeur = ModePropreObs.Valeur*(1+5*10-2*rand(1,1));
-    delta.w2=ModePropreObs.Bruit2.Valeur-ModePropreNorme.Valeur';
-    Vecteurp.recons_bruit2=cgs(matriceR.Rw,delta.w2,tol);
-    Vecteurp_diff.bruit2=Vecteurp.recons_bruit2-Vecteurp.orig;
-    Vecteurp_relat.bruit2=zeros(donnee.nelem,1);
-    for i=1:donnee.nelem
-        Vecteurp_relat.bruit2(i)=Vecteurp_diff.bruit2(i)/Vecteurp.orig(i);
-    end
-    % Entre 0 et 10%
-   
-    ModePropreObs.Bruit3.Valeur = ModePropreObs.Valeur*(1+10^-1*rand(1,1));
-    delta.w3=ModePropreObs.Bruit3.Valeur-ModePropreNorme.Valeur';
-    Vecteurp.recons_bruit3=cgs(matriceR.Rw,delta.w3,tol);
-    Vecteurp_diff.bruit3=Vecteurp.recons_bruit3-Vecteurp.orig;
-    Vecteurp_relat.bruit3=zeros(donnee.nelem,1);
-    for i=1:donnee.nelem
-        Vecteurp_relat.bruit3(i)=Vecteurp_diff.bruit3(i)/Vecteurp.orig(i);
-    end
-    
-    
-    % Entre 0 et 20%
-   
-    ModePropreObs.Bruit4.Valeur = ModePropreObs.Valeur*(1+0.2*rand(1,1));
-    delta.w4=ModePropreObs.Bruit4.Valeur-ModePropreNorme.Valeur';
-    Vecteurp.recons_bruit4=cgs(matriceR.Rw,delta.w4,tol);
-    Vecteurp_diff.bruit4=Vecteurp.recons_bruit4-Vecteurp.orig;
-    Vecteurp_relat.bruit4=zeros(donnee.nelem,1);
-    for i=1:donnee.nelem
-        Vecteurp_relat.bruit4(i)=Vecteurp_diff.bruit4(i)/Vecteurp.orig(i);
-    end
-    
-    % Entre 0 et 100%
-    
-    ModePropreObs.Bruit5.Valeur = ModePropreObs.Valeur*(1+0.5*rand(1,1));
-    delta.w5=ModePropreObs.Bruit5.Valeur-ModePropreNorme.Valeur';
-    Vecteurp.recons_bruit5=cgs(matriceR.Rw,delta.w5,tol);
-    Vecteurp_diff.bruit5=Vecteurp.recons_bruit5-Vecteurp.orig;
-    Vecteurp_relat.bruit5=zeros(donnee.nelem,1);
-    for i=1:donnee.nelem
-        Vecteurp_relat.bruit5(i)=Vecteurp_diff.bruit5(i)/Vecteurp.orig(i);
-    end
-    
     %%Affichage
     Vect_comp=zeros(donnee.nelem,6);
     Vect_comp(:,1)=Vecteurp_diff.orig;
@@ -154,8 +164,10 @@ clear all;close all;clc;
     figure;
     %plot(x,Vecteurp.recons,'b',x,Vecteurp.orig,'g',x,Vecteurp.recons_bruit1,'r');%,x,Vecteurp_recons_bruit1,'c',x,Vecteurp_recons_bruit2,'m',x,Vecteurp_recons_bruit3,'y',x,Vecteurp_recons_bruit4,'k');
     bar(x,Vect_comp,1.5)
+    title('p=E/E0 reconstruit');
     figure;
     bar(x,Vect_relat,1.5)
+    title('erreur relative sur p=E/E0 reconstruit');
     
     
     
